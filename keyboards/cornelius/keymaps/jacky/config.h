60eb4403321dce4636b8c0373c8e5101532a72a9
Jacky Chen <jacky@Jackys-Mac-Studio.local>
2026-01-16 20:20:31 -0600

    updated cornelius keymap

--- keyboards/cornelius/keymaps/jacky/config.h
+++ keyboards/cornelius/keymaps/jacky/config.h
@@ -1,7 +1,12 @@
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 135
-#define FLOW_TAP_TERM 75
+#define TAPPING_TERM 145
+#define FLOW_TAP_TERM 30
 
 // Enable rapid switch rom tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 #define PERMISSIVE_HOLD
+
+
+// Mouse
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_MAX_SPEED 2
--- keyboards/cornelius/keymaps/jacky/keymap.c
+++ keyboards/cornelius/keymaps/jacky/keymap.c
@@ -28,47 +28,42 @@
 #define HOME_I LALT_T(KC_I)
 
 
-const uint16_t PROGMEM copy_combo[] = {KC_C, KC_D, COMBO_END};
-const uint16_t PROGMEM paste_combo[] = {KC_D, KC_V, COMBO_END};
-const uint16_t PROGMEM colon_combo[] = {KC_Y, KC_QUOT, COMBO_END};
-const uint16_t PROGMEM semicolon_combo[] = {KC_Y, KC_U, COMBO_END};
-const uint16_t PROGMEM caps_word_combo[] = {KC_T, KC_N, COMBO_END};
-
+const uint16_t PROGMEM colon_combo[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM caps_word_combo[] = {HOME_T, HOME_N, COMBO_END};
+const uint16_t PROGMEM bang_combo[] = { KC_F, KC_P, COMBO_END};
+const uint16_t PROGMEM underscore_combo[] = { KC_H, KC_COMMA, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(copy_combo, KC_COPY),
-    COMBO(paste_combo, KC_PASTE),
     COMBO(colon_combo, KC_COLN),
-    COMBO(semicolon_combo, KC_SCLN),
-    COMBO(caps_word_combo, CW_TOGG)
+    COMBO(bang_combo, KC_EXLM),
+    COMBO(caps_word_combo, CW_TOGG),
+    COMBO(underscore_combo, KC_UNDS),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-     _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,       KC_J,           KC_L,          KC_U,     KC_Y,    KC_QUOT, _______,
-     _______, HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,       KC_M,           HOME_N,        HOME_E,   HOME_I,  HOME_O, _______,
-     _______, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,       KC_K,           KC_H,          KC_COMMA, KC_DOT,  LT(3, KC_SLSH), _______,
-     _______, _______, _______, LT(5, KC_ESC),  LT(3, KC_SPC),  SFT_T(KC_TAB),     KC_ENT,  LT(1, KC_BSPC), LT(2, KC_DEL), _______,  _______, _______
+     KC_GRV, KC_Q, KC_W,    KC_F,    KC_P,    KC_B,    KC_J,   KC_L,          KC_U,          KC_Y,    KC_SCLN, _______,
+     CTL_T(KC_ESC), HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,   KC_M,          HOME_N,        HOME_E,   HOME_I,  HOME_O, KC_QUOT,
+     _______, KC_Z, KC_X,    KC_C,    KC_D,    KC_V,    KC_K,   KC_H,          KC_COMMA,      KC_DOT,  LT(3, KC_SLSH), _______,
+     _______, _______, _______, LT(5, KC_ESC),  LT(1, KC_SPC),  LT(3, KC_TAB), LT(1, KC_ENT), LT(2, KC_BSPC), LT(2, KC_DEL), _______,  _______, _______
   ),
-
   [1] = LAYOUT( // Numbers
      _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
      _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
      _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,  _______
   ),
-
   [2] = LAYOUT( // Symbols
-     _______, KC_GRV, _______, _______, KC_PERC, KC_EXLM,     KC_PIPE, KC_PLUS,  KC_LPRN, KC_RPRN,  KC_LT,   _______,
-     _______, KC_AMPR, KC_AT, KC_ASTR, _______, _______,      KC_MINS, KC_EQUAL, KC_LBRC, KC_RBRC,  KC_GT,   _______,
-     _______, _______, _______, KC_CIRC, KC_DOLLAR, _______,  _______, KC_UNDS,  KC_LCBR, KC_RCBR,  KC_BSLS, _______,
+     _______, KC_GRV, KC_DOLLAR, KC_HASH, KC_PERC, KC_EXLM,   KC_PIPE, KC_PLUS,  KC_LPRN, KC_RPRN,  KC_LT,   KC_GT,
+     _______, KC_AMPR, KC_AT, KC_ASTR, _______, KC_GRV,       _______, KC_EQUAL, KC_LBRC, KC_RBRC,  KC_GT,   KC_DQT,
+     _______, KC_PERC, KC_EXLM, KC_CIRC, KC_DOLLAR, _______,  _______, KC_MINS,  KC_LCBR, KC_RCBR,  KC_BSLS, _______,
      _______, _______, _______, _______, _______, _______,    _______, _______, _______,  _______, _______,  _______
   ),
   [3] = LAYOUT( // Navigation
      _______, _______, _______, MS_UP,   _______, _______, _______, _______, _______, _______,  _______, _______,
      _______, _______, MS_LEFT, MS_DOWN, MS_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
      _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   _______, _______,
-     _______, _______, _______, MS_BTN3, MS_BTN1, MS_BTN2, _______, _______, _______, _______, _______,  _______
+     _______, _______, _______, _______, _______, _______, MS_BTN2, MS_BTN1, MS_BTN3, _______, _______, _______
   ),
   [4] = LAYOUT(
      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
